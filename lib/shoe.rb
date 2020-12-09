6f094fa5389c18698d7491c46154d544e7a14908
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-09 15:57:08 -0600

    commit tree 10042b6df76a273a1e1d02644298349b688baee9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    def initialize (brand)
+        @brand = brand
+    end
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+    
+
+    def cobble
+    #binding.pry
+         @condition = "new"
+         #binding.pry
+        puts "Your shoe is as good as new!"
+    end
+   #binding.pry
+end
+
+#binding.pry
